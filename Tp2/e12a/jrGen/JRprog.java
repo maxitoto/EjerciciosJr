361d88cfd0045ca0991eb0b535ab4cf04bcde59e
maxitoto <tommyush2000@hotmail.com>
2024-05-05 03:22:10 -0300

    Ejercicio vulnerabilidad simple hecho

---
+++ Tp2/e12a/jrGen/JRprog.java
@@ -0,0 +1,134 @@
+/*****************************************************
+ * jr generated file
+ ****************************************************/
+import edu.ucdavis.jr.*;
+import edu.ucdavis.jr.jrx.*;
+import java.rmi.*;
+import java.io.Serializable;
+
+    public class JRprog
+      extends JRjavadotlangdotObject
+      implements Serializable, Cloneable
+    {
+	static final long serialVersionUID = 0;
+	public Cap_ext_ op_rojo_intTovoid;
+	public Cap_ext_ JRget_op_rojo_intTovoid() {
+		return op_rojo_intTovoid;
+	}
+	public void JRset_op_rojo_intTovoid(Cap_ext_ op_rojo_intTovoid) {
+		this.op_rojo_intTovoid = op_rojo_intTovoid;
+	}
+
+	public Cap_ext_ op_blanco_intTovoid;
+	public Cap_ext_ JRget_op_blanco_intTovoid() {
+		return op_blanco_intTovoid;
+	}
+	public void JRset_op_blanco_intTovoid(Cap_ext_ op_blanco_intTovoid) {
+		this.op_blanco_intTovoid = op_blanco_intTovoid;
+	}
+
+	public Cap_ext_ op_X_voidTovoid;
+	public Cap_ext_ JRget_op_X_voidTovoid() {
+		return op_X_voidTovoid;
+	}
+	public void JRset_op_X_voidTovoid(Cap_ext_ op_X_voidTovoid) {
+		this.op_X_voidTovoid = op_X_voidTovoid;
+	}
+
+	public Cap_ext_ op_mutexR_voidTovoid;
+	public Cap_ext_ JRget_op_mutexR_voidTovoid() {
+		return op_mutexR_voidTovoid;
+	}
+	public void JRset_op_mutexR_voidTovoid(Cap_ext_ op_mutexR_voidTovoid) {
+		this.op_mutexR_voidTovoid = op_mutexR_voidTovoid;
+	}
+
+	public Cap_ext_ op_mutexB_voidTovoid;
+	public Cap_ext_ JRget_op_mutexB_voidTovoid() {
+		return op_mutexB_voidTovoid;
+	}
+	public void JRset_op_mutexB_voidTovoid(Cap_ext_ op_mutexB_voidTovoid) {
+		this.op_mutexB_voidTovoid = op_mutexB_voidTovoid;
+	}
+
+	public Cap_ext_ op_morir_voidTovoid;
+	public Cap_ext_ JRget_op_morir_voidTovoid() {
+		return op_morir_voidTovoid;
+	}
+	public void JRset_op_morir_voidTovoid(Cap_ext_ op_morir_voidTovoid) {
+		this.op_morir_voidTovoid = op_morir_voidTovoid;
+	}
+
+	public Cap_ext_ op_bloquear_voidTovoid;
+	public Cap_ext_ JRget_op_bloquear_voidTovoid() {
+		return op_bloquear_voidTovoid;
+	}
+	public void JRset_op_bloquear_voidTovoid(Cap_ext_ op_bloquear_voidTovoid) {
+		this.op_bloquear_voidTovoid = op_bloquear_voidTovoid;
+	}
+
+	public Cap_ext_ op_esperaR_voidTovoid;
+	public Cap_ext_ JRget_op_esperaR_voidTovoid() {
+		return op_esperaR_voidTovoid;
+	}
+	public void JRset_op_esperaR_voidTovoid(Cap_ext_ op_esperaR_voidTovoid) {
+		this.op_esperaR_voidTovoid = op_esperaR_voidTovoid;
+	}
+
+	public Cap_ext_ op_esperaB_voidTovoid;
+	public Cap_ext_ JRget_op_esperaB_voidTovoid() {
+		return op_esperaB_voidTovoid;
+	}
+	public void JRset_op_esperaB_voidTovoid(Cap_ext_ op_esperaB_voidTovoid) {
+		this.op_esperaB_voidTovoid = op_esperaB_voidTovoid;
+	}
+
+	public JRprog(JRprog copy)
+	{
+	this.op_rojo_intTovoid = copy.op_rojo_intTovoid;
+	this.op_blanco_intTovoid = copy.op_blanco_intTovoid;
+	this.op_X_voidTovoid = copy.op_X_voidTovoid;
+	this.op_mutexR_voidTovoid = copy.op_mutexR_voidTovoid;
+	this.op_mutexB_voidTovoid = copy.op_mutexB_voidTovoid;
+	this.op_morir_voidTovoid = copy.op_morir_voidTovoid;
+	this.op_bloquear_voidTovoid = copy.op_bloquear_voidTovoid;
+	this.op_esperaR_voidTovoid = copy.op_esperaR_voidTovoid;
+	this.op_esperaB_voidTovoid = copy.op_esperaB_voidTovoid;
+
+	}
+	public JRprog(Object ... opSig)
+	{
+	this.op_rojo_intTovoid = new Cap_ext_((Op_ext_.JRProxyOp)opSig[0]);
+	this.op_blanco_intTovoid = new Cap_ext_((Op_ext_.JRProxyOp)opSig[1]);
+	this.op_X_voidTovoid = new Cap_ext_((Op_ext_.JRProxyOp)opSig[2]);
+	this.op_mutexR_voidTovoid = new Cap_ext_((Op_ext_.JRProxyOp)opSig[3]);
+	this.op_mutexB_voidTovoid = new Cap_ext_((Op_ext_.JRProxyOp)opSig[4]);
+	this.op_morir_voidTovoid = new Cap_ext_((Op_ext_.JRProxyOp)opSig[5]);
+	this.op_bloquear_voidTovoid = new Cap_ext_((Op_ext_.JRProxyOp)opSig[6]);
+	this.op_esperaR_voidTovoid = new Cap_ext_((Op_ext_.JRProxyOp)opSig[7]);
+	this.op_esperaB_voidTovoid = new Cap_ext_((Op_ext_.JRProxyOp)opSig[8]);
+
+	}
+	public JRprog(boolean dummy)	{
+	    super(dummy);
+	this.op_rojo_intTovoid = Cap_ext_.noop;
+	this.op_blanco_intTovoid = Cap_ext_.noop;
+	this.op_X_voidTovoid = Cap_ext_.noop;
+	this.op_mutexR_voidTovoid = Cap_ext_.noop;
+	this.op_mutexB_voidTovoid = Cap_ext_.noop;
+	this.op_morir_voidTovoid = Cap_ext_.noop;
+	this.op_bloquear_voidTovoid = Cap_ext_.noop;
+	this.op_esperaR_voidTovoid = Cap_ext_.noop;
+	this.op_esperaB_voidTovoid = Cap_ext_.noop;
+
+	}
+	public Object clone()
+	    throws CloneNotSupportedException
+	{
+	    return super.clone();
+	}
+	public static Object getNoop()
+	{
+	    return new JRprog(true);
+	}
+    }
